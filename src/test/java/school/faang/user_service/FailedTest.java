961ddc917d1e0e3bf1ac2484882801a38686fe5d
Anton <basanton@mail.ru>
2025-03-14 11:06:14 +0300

    Падающий тест для проверки CI

--- src/test/java/school/faang/user_service/FailedTest.java
+++ src/test/java/school/faang/user_service/FailedTest.java
@@ -2,11 +2,16 @@ package school.faang.user_service;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class FailedTest {
-    @Test
     public void testThatShouldFail() {
         fail("Этот тест должен упасть");
+    }
+
+    @Test
+    public void testThatAlwaysPasses() {
+        assertTrue(true, "Этот тест всегда должен проходить");
     }
 }
